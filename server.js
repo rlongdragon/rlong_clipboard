79b0879f24e0e878c88b9b9769a37fa47181536c
rlongdragon <jz744335@gmail.com>
2025-03-09 14:08:17 +0000

    新增 cors 套件以支援跨來源請求

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json({ limit: '20kb' })); // Limit request body size to 10kb
 
